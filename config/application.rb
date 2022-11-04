aff22abc8418c2c188555749cb455c76081d76e6
Manish Joshi <mj3.14manish@gmail.com>
2022-11-04 10:09:35 +0530

    property update

--- config/application.rb
+++ config/application.rb
@@ -18,16 +18,17 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+#config.assets.initialize_on_precompile = false
 module UrlHashing
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.domain_path =  'https://url-hashing-91.herokuapp'
+    config.domain_path =  'http://0.0.0.0:3000'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.initialize_on_precompile = false
 
     # Don't generate system test files.
     config.generators.system_tests = nil
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  config.domain_path =  'http://url-hashing-9.herokuapp'
+  config.domain_path =  'http://0.0.0.0:3000'
 
 
   # Do not eager load code on boot.
